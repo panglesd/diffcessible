79edd07841f2fbaf84851f5b9523cc2d67a7a959
Alan Matthew <alan.matthew.yk@gmail.com>
2024-03-05 02:20:45 +0800

    Accept the patch from a file as a required command line argument (#22)

--- bin/main.ml
+++ bin/main.ml
@@ -1,16 +1,19 @@
 open Diffcessible
+open Cmdliner
 
-let main () =
-  let file = In_channel.open_bin "diff.example" in
+let main file_path =
+  let file = In_channel.open_bin file_path in
   let s = In_channel.input_all file in
   let patch = Patch.to_diffs s in
   Interactive_viewer.start patch
 
-open Cmdliner
+let file_arg =
+  let doc = "Path to the file containing the Git diff." in
+  Arg.(required & pos 0 (some string) None & info [] ~docv:"FILE" ~doc)
 
 let cmd =
   let doc = "Render Git diffs in an accessible way." in
   let info = Cmd.info "diffcessible" ~version:"VERSION" ~doc in
-  Cmd.v info Term.(const main $ const ())
+  Cmd.v info Term.(const main $ file_arg)
 
 let () = exit (Cmd.eval cmd)

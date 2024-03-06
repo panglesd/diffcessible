4b87b631110ccd3d28ebff73d369515aedb64fbe
Neba Desmond <desmondneba134@gmail.com>
2024-03-06 11:41:10 +0100

    Improve current operation display format (#27)
    Display current operation in status bar in a more readable form

--- lib/interactive_viewer.ml
+++ lib/interactive_viewer.ml
@@ -9,13 +9,47 @@ let current_patch patches =
   let$ index = index in
   List.nth_opt patches index
 
-let string_of_operation = Format.asprintf "%a" (Patch.pp_operation ~git:false)
+let ui_of_operation operation =
+  let green_bold_attr = Notty.A.(fg green ++ st bold) in
+  let red_bold_attr = Notty.A.(fg red ++ st bold) in
+  let blue_bold_attr = Notty.A.(fg blue ++ st bold) in
+  match operation with
+  | Patch.Create path ->
+    Ui.hcat [
+          W.string "Creation of ";
+          W.string ~attr:green_bold_attr path; 
+    ]
+  | Patch.Delete path ->
+    Ui.hcat [
+      W.string "Deletion of ";
+      W.string ~attr:red_bold_attr path;
+    ]
+  | Patch.Rename (old_path, new_path) ->
+    Ui.hcat [
+      W.string "Rename with modifications ";
+      W.string ~attr:blue_bold_attr old_path;
+      W.string " to ";
+      W.string ~attr:green_bold_attr new_path;
+    ]
+  | Patch.Rename_only (old_path, new_path) ->
+    Ui.hcat [
+      W.string "Rename ";
+      W.string ~attr:blue_bold_attr old_path;
+      W.string " to ";
+      W.string ~attr:green_bold_attr new_path;
+    ]
+  | Patch.Edit path ->
+    Ui.hcat [
+      W.string "Modification of ";
+      W.string ~attr:blue_bold_attr path;
+    ]
+
 let string_of_hunk = Format.asprintf "%a" Patch.pp_hunk
 
 let current_operation patches =
   let$ current_patch = current_patch patches in
   match current_patch with
-  | Some p -> W.string @@ string_of_operation p.Patch.operation
+  | Some p -> ui_of_operation p.Patch.operation
   | None -> W.string "No operation"
 
 let current_hunks patches =

dcbecc4df0a74c7654fc75f14f02220c8073214d
Mahalakshmi <mahalaxmidev.1019@gmail.com>
2024-03-11 11:27:11 -0400

    Show number of lines added and removed  (#30)
    Co-authored-by: maha-sachin <mahasachin108@gmail.com>

--- lib/interactive_viewer.ml
+++ lib/interactive_viewer.ml
@@ -60,8 +60,40 @@ let navigate z_patches (dir : direction) : unit =
   | Prev -> Lwd.set z_patches (Zipper.prev z)
   | Next -> Lwd.set z_patches (Zipper.next z)
 
-(* let pure_str s = Lwd.pure (W.string s) *)
 let quit = Lwd.var false
+
+let additions_and_removals lines =
+  let add_line (additions, removals) line =
+    match line with
+    | `Their _ -> (additions + 1, removals)
+    | `Mine _ -> (additions, removals + 1)
+    | `Common _ -> (additions, removals)
+  in
+  List.fold_left add_line (0, 0) lines
+
+let accumulate_count hunks =
+  List.fold_left
+    (fun (add_acc, remove_acc) hunk ->
+      let add_in_hunk, remove_in_hunk =
+        additions_and_removals hunk.Patch.lines
+      in
+      (add_acc + add_in_hunk, remove_acc + remove_in_hunk))
+    (0, 0) hunks
+
+let change_summary z_patches : ui Lwd.t =
+  let$ z = Lwd.get z_patches in
+  let p = Zipper.get_focus z in
+  let total_additions, total_removals = accumulate_count p.Patch.hunks in
+  let format_plural n singular plural =
+    if n = 1 then Printf.sprintf "%d %s" n singular
+    else Printf.sprintf "%d %s" n plural
+  in
+  let operation_count =
+    Printf.sprintf "%s, %s"
+      (format_plural total_additions "addition" "additions")
+      (format_plural total_removals "removal" "removals")
+  in
+  W.string ~attr:Notty.A.(fg lightcyan) operation_count
 
 let view (patches : Patch.t list) =
   let z_patches : 'a Zipper.t Lwd.var =
@@ -72,6 +104,7 @@ let view (patches : Patch.t list) =
   W.vbox
     [
       operation_info z_patches;
+      change_summary z_patches;
       current_operation z_patches;
       W.scrollbox @@ current_hunks z_patches;
       Lwd.pure

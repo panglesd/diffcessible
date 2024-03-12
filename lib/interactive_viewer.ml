1ef92e4f2cc0e051e11d7b418ba2eb2773fc37cd
Siddhi Agrawal <siddhiagrawalcool@gmail.com>
2024-03-12 22:32:08 +0530

    made improvements in the code -changed pattern matching to if..then..else -improved Lwd.bind syntax -shifted help panel ui to top level -changed q to quit the help panel -formatted the code

--- lib/interactive_viewer.ml
+++ lib/interactive_viewer.ml
@@ -31,62 +31,63 @@ let help = Lwd.var false
 
 <<<<<<< HEAD
 let view (patches : Patch.t list) =
+  let help_panel =
+    Ui.vcat
+      [
+        W.string "Help Panel:\n";
+        W.string "h:   Toggle the help panel";
+        W.string "q:   Quit the diffcessible viewer";
+        W.string "n:   Move to the next operation, if present";
+        W.string "p:   Move to the previous operation, if present";
+      ]
+  in
   let z_patches : 'a Zipper.t Lwd.var =
     match Zipper.zipper_of_list patches with
     | Some z -> Lwd.var z
     | None -> failwith "zipper_of_list: empty list"
   in
-  let help_panel = Ui.vcat
-  [
-     W.string "h:  This key can be used to toggle the help panel";
-     W.string "q:  This key can be used to quit the diffcessible viewer";
-     W.string "n:  This key can be used to move to the next operation, if present";
-     W.string "p:  This key can be used to move to the previous operation, if present" ;
-  ] in
-  let help = Lwd.var false  in
-  let help_visible=Lwd.get help in
-  if Lwd.peek help_visible then W.vbox[
-  Lwd.pure @@ help_panel;
-  Lwd.pure
-      @@ Ui.keyboard_area
-           (function
-             | `ASCII 'q', [] ->
-                 Lwd.set quit true;
-                 `Handled
-             | `ASCII 'h', [] ->
-                 Lwd.set help_visible false;             
-                 `Handled
-             | _ -> `Unhandled)
-            (W.string "Type 'q' to quit, 'h' to exit the help panel");
-
-   ]
-  else
-  W.vbox
-    [
-      current_operation patches;
-      W.scrollbox @@ current_hunks patches;
-      Lwd.pure
-      @@ Ui.keyboard_area
-           (function
-             | `ASCII 'q', [] ->
-                 Lwd.set quit true;
-                 `Handled
-             | `ASCII 'n', [] ->
-                 Lwd.set index_v (Lwd.peek index_v + 1);
-                 `Handled
-             | `ASCII 'p', [] ->
-                 navigate z_patches Prev;
-                 `Handled
-             | `ASCII 'h', [] ->
-                 Lwd.set help_visible true;
-                 `Handled
-             | `ASCII 'h', [] ->
-                 Lwd.set help true;
-                 `Handled
-             | _ -> `Unhandled)
-           (W.string
-              "Type 'q' to quit, 'n' to go to the next operation, 'p' to go to \
-               the previous operation, 'h' to go to the help panel");
-    ]
+  let ui =
+    let$* help_visible = Lwd.get help in
+    if help_visible then
+      W.vbox
+        [
+          W.scrollbox @@ Lwd.pure @@ help_panel;
+          Lwd.pure
+          @@ Ui.keyboard_area
+               (function
+                 | `ASCII 'q', [] ->
+                     Lwd.set help false;
+                     `Handled
+                 | _ -> `Unhandled)
+               (W.string "Type 'q' to exit the help panel");
+        ]
+    else
+      W.vbox
+        [
+          operation_info z_patches;
+          current_operation z_patches;
+          W.scrollbox @@ current_hunks z_patches;
+          Lwd.pure
+          @@ Ui.keyboard_area
+               (function
+                 | `ASCII 'q', [] ->
+                     Lwd.set quit true;
+                     `Handled
+                 | `ASCII 'n', [] ->
+                     navigate z_patches Next;
+                     `Handled
+                 | `ASCII 'p', [] ->
+                     navigate z_patches Prev;
+                     `Handled
+                 | `ASCII 'h', [] ->
+                     Lwd.set help true;
+                     `Handled
+                 | _ -> `Unhandled)
+               (W.string
+                  "Type 'q' to quit, 'n' to go to the next operation, 'p' to \
+                   go to the previous operation, 'h' to go to the help panel");
+        ]
+  in
+  W.vbox [ ui ]
 
 let start patch = Ui_loop.run ~quit ~tick_period:0.2 (view patch)

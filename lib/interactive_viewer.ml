a829a5ae5029417dd97b167e0926f282c47ef9e2
Siddhi Agrawal <siddhiagrawalcool@gmail.com>
2024-03-13 03:18:09 +0530

    added Content handling in change fun for vscroll_area

--- lib/interactive_viewer.ml
+++ lib/interactive_viewer.ml
@@ -72,15 +72,8 @@ let view (patches : Patch.t list) =
   let curr_scroll_state = Lwd.var W.default_scroll_state in
   let change_scroll_state action state =
     let off_screen = action = `Action && state.W.position > state.W.bound in
-    let scroll_on_output =
-      action = `Action
-      &&
-      let state' = Lwd.peek curr_scroll_state in
-      state'.W.position = state'.W.bound
-      && state.W.position = state'.W.position
-      && state.W.position < state.W.bound
-    in
-    if scroll_on_output || off_screen then
+    let off_screen_content = action = `Content && state.W.position > state.W.bound in
+    if off_screen || off_screen_content then
       Lwd.set curr_scroll_state { state with position = state.W.bound }
     else Lwd.set curr_scroll_state state
   in

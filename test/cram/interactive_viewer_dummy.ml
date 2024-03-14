9d13216d5f7efce73744c9c2b887952cc43c0419
Desmond Neba <desmondneba134@gmail.com>
2024-03-14 11:40:01 +0100

    format code

--- test/cram/interactive_viewer_dummy.ml
+++ test/cram/interactive_viewer_dummy.ml
@@ -4,39 +4,39 @@ let input_events : Notty.Unescape.event list ref = ref []
 let output_image : Notty.image ref = ref Notty.I.empty
 
 let print_image () : unit =
-  let main_image = Notty.I.vcat [Notty.I.string Notty.A.empty "Hello World" ] in
+  let main_image =
+    Notty.I.vcat [ Notty.I.string Notty.A.empty "Hello World" ]
+  in
   output_image := main_image;
   Notty_unix.output_image ~fd:stdout !output_image;
 
   print_endline ""
 
-
-
 let run_dummy_ui () : unit =
   let rec loop events =
     match events with
     | [] -> ()
-    | event :: rest ->
-      match event with
-      | `Key (`ASCII 'q', []) -> ()
-      | _ ->
-        print_image ();
-        loop rest
+    | event :: rest -> (
+        match event with
+        | `Key (`ASCII 'q', []) -> ()
+        | _ ->
+            print_image ();
+            loop rest)
   in
   print_image ();
   loop !input_events
-  
+
 let main (_input_events : char list) =
   input_events := List.map (fun c -> `Key (`ASCII c, [])) _input_events;
   run_dummy_ui ()
 
 let input_events_term =
   let doc = "Input event as a single character" in
- Arg.(value & pos_all char [] & info [] ~docv:"EVENT" ~doc)
+  Arg.(value & pos_all char [] & info [] ~docv:"EVENT" ~doc)
 
 let cmd =
   let doc = "Interactive Viewer Dummy" in
   let info = Cmd.info "interactive_viewer_dummy" ~version:"VERSION" ~doc in
-  Cmd.v info Term.(const main $  input_events_term)
-  
+  Cmd.v info Term.(const main $ input_events_term)
+
 let () = exit @@ Cmd.eval cmd

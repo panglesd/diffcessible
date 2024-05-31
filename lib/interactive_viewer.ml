84fd92a915a0e4219edd1c0a143f066b061209d4
You Guang <yokurang@gmail.com>
2024-06-01 01:31:26 +0800

    adding togglable lines

--- lib/interactive_viewer.ml
+++ lib/interactive_viewer.ml
@@ -47,14 +47,24 @@ let ui_of_operation operation =
       Ui.hcat
         [ W.string "Modification of "; W.string ~attr:blue_bold_attr path ]
 
+let line_numbers_visible = Lwd.var false
+
+let toggle_line_numbers () =
+  let current = Lwd.peek line_numbers_visible in
+  Lwd.set line_numbers_visible (not current)
+
 let ui_of_hunk hunk =
-  let line_to_string line =
+  let line_to_string i line =
+    let line_number =
+      if Lwd.peek line_numbers_visible then Printf.sprintf "%4d " (i + 1)
+      else ""
+    in
     match line with
-    | `Their text -> W.string ~attr:Notty.A.(fg red) text
-    | `Mine text -> W.string ~attr:Notty.A.(fg green) text
-    | `Common text -> W.string text
+    | `Their text -> W.string ~attr:Notty.A.(fg red) (line_number ^ text)
+    | `Mine text -> W.string ~attr:Notty.A.(fg green) (line_number ^ text)
+    | `Common text -> W.string (line_number ^ text)
   in
-  Ui.vcat @@ List.map line_to_string hunk.Patch.lines
+  Ui.vcat @@ List.mapi line_to_string hunk.Patch.lines
 
 let current_operation z_patches : ui Lwd.t =
   let$ z = Lwd.get z_patches in
@@ -136,7 +146,7 @@ let rec split_and_align_hunk hunks mine_acc their_acc =
 
 (* Function to wrap text to the specified width with consistent spacing *)
 let wrap_text max_width s =
-  let rec aux acc curr line =
+  let rec aux acc line =
     if String.length line <= max_width then List.rev (line :: acc)
     else
       let split_index =
@@ -147,12 +157,15 @@ let wrap_text max_width s =
       let after =
         String.sub line (split_index + 1) (String.length line - split_index - 1)
       in
-      aux (before :: acc) (curr + 1) after
+      aux (before :: acc) after
   in
-  aux [] 0 s
+  aux [] s
 
-(* Function to convert line diffs to UI elements with consistent alignment *)
-let lines_to_ui lines attr_line_number attr_change max_width =
+(* Function to convert line diffs to UI elements with consistent alignment and continuous line numbering, updated to handle line wraps *)
+let lines_to_ui lines attr_line_number attr_change max_width
+    starting_line_number =
+  let line_number_ref = ref starting_line_number in
+  (* Use a reference to track the line number *)
   List.flatten
     (List.map
        (fun line ->
@@ -163,22 +176,40 @@ let lines_to_ui lines attr_line_number attr_change max_width =
            | `Their s -> (s, attr_change)
          in
          let wrapped_lines = wrap_text max_width content in
+         (* Generate UI for each wrapped line, incrementing the line number for each *)
          List.map
-           (fun l -> W.string ~attr (Printf.sprintf "%-*s" max_width l))
+           (fun wrapped_line ->
+             let line_number_str =
+               if Lwd.peek line_numbers_visible then
+                 Printf.sprintf "%4d " !line_number_ref
+               else ""
+             in
+             let ui = W.string ~attr (line_number_str ^ wrapped_line) in
+             incr line_number_ref;
+             (* Increment line number for each new line *)
+             ui)
            wrapped_lines)
        lines)
 
-(* Function to create side-by-side UI from a hunk *)
-let ui_of_hunk_side_by_side hunk max_width =
+(* Function to create side-by-side UI from a hunk, updated with continuous line numbering *)
+let ui_of_hunk_side_by_side hunk max_width starting_line_number =
   let mine_lines, their_lines = split_and_align_hunk hunk.Patch.lines [] [] in
 
   let attr_line_number = Notty.A.(fg lightblue) in
   let attr_mine = Notty.A.(fg red ++ st bold) in
   let attr_their = Notty.A.(fg green ++ st bold) in
 
-  let mine_ui = lines_to_ui mine_lines attr_line_number attr_mine max_width in
+  let adjusted_width =
+    if Lwd.peek line_numbers_visible then max_width - 5 else max_width
+  in
+
+  let mine_ui =
+    lines_to_ui mine_lines attr_line_number attr_mine adjusted_width
+      starting_line_number
+  in
   let their_ui =
-    lines_to_ui their_lines attr_line_number attr_their max_width
+    lines_to_ui their_lines attr_line_number attr_their adjusted_width
+      starting_line_number
   in
 
   let space = Ui.space 1 0 in
@@ -189,13 +220,29 @@ let ui_of_hunk_side_by_side hunk max_width =
       Ui.resize ~sw:1 (Ui.vcat their_ui);
     ]
 
+let count_lines_in_hunk hunk =
+  List.fold_left
+    (fun total_lines line ->
+      match line with
+      | `Common _ -> total_lines + 1
+      | `Mine _ -> total_lines + 1
+      | `Their _ -> total_lines + 1)
+    0 hunk.Patch.lines
+
 let current_hunks_side_by_side z_patches max_width : ui Lwd.t =
   let$ z = Lwd.get z_patches in
   let p = Zipper.get_focus z in
-  let hunks_ui =
-    List.map (fun h -> ui_of_hunk_side_by_side h max_width) p.Patch.hunks
+  let initial_line_number = 1 in
+  let hunks_ui, last_line_number =
+    List.fold_left
+      (fun (acc_ui, current_line) hunk ->
+        let hunk_ui = ui_of_hunk_side_by_side hunk max_width current_line in
+        let lines_in_hunk = count_lines_in_hunk hunk in
+        (hunk_ui :: acc_ui, current_line + lines_in_hunk))
+      ([], initial_line_number) p.Patch.hunks
   in
-  Ui.vcat @@ hunks_ui
+  Printf.printf "Last line number used: %d\n" last_line_number;
+  Ui.vcat (List.rev hunks_ui)
 
 (** end of side by side diff view implementation **)
 
@@ -218,7 +265,6 @@ let view (patches : Patch.t list) =
     | Some z -> Lwd.var z
     | None -> failwith "zipper_of_list: empty list"
   in
-  (* Use the mutable width variable here *)
   let max_width = !dynamic_width in
   let hunks_ui =
     Lwd.bind (Lwd.get view_mode) ~f:(fun mode ->
@@ -275,11 +321,15 @@ let view (patches : Patch.t list) =
                  | `ASCII 't', [] ->
                      toggle_view_mode ();
                      `Handled
+                 | `ASCII 'l', [] ->
+                     toggle_line_numbers ();
+                     `Handled
                  | _ -> `Unhandled)
                (W.string
                   "Type 'h' to go to the help panel, 'q' to quit, 'n' to go to \
                    the next operation, 'p' to go to the previous operation. \
-                   Press 't' to toggle view mode.");
+                   Press 't' to toggle view mode. Press 'l' to toggle line \
+                   numbers.");
         ]
   in
 
@@ -338,3 +388,4 @@ let start_test patch events width height =
   Notty_unix.output_image init_image;
   Lwd.quick_release content_ui_root;
   print_newline ()
+

6b2390379d0d5171c73507b895212ffa95a1ebf1
You Guang <yokurang@gmail.com>
2024-06-15 20:10:19 +0800

    added comments

--- bin/main.ml
+++ bin/main.ml
@@ -1,19 +1,55 @@
 open Diffcessible
-open Cmdliner
+open Async
 
-let main file_path =
-  let file = In_channel.open_bin file_path in
-  let s = In_channel.input_all file in
-  let patch = Patch.to_diffs s in
+let read_content file_path_opt =
+  match file_path_opt with
+  | Some path ->
+      let file = In_channel.open_bin path in
+      let content = In_channel.input_all file in
+      In_channel.close file;
+      return content
+  | None -> Reader.contents (Lazy.force Reader.stdin)
+
+let process_diffs content =
+  let patch = Patch.to_diffs content in
   Interactive_viewer.start patch
 
-let file_arg =
-  let doc = "Path to the file containing the Git diff." in
-  Arg.(required & pos 0 (some string) None & info [] ~docv:"FILE" ~doc)
+let main file_path_opt () =
+  let open Deferred.Let_syntax in
+  let%bind content = read_content file_path_opt in
+  process_diffs content;
+  return ()
 
-let cmd =
-  let doc = "Render Git diffs in an accessible way." in
-  let info = Cmd.info "diffcessible" ~version:"VERSION" ~doc in
-  Cmd.v info Term.(const main $ file_arg)
+let command =
+  let open Command.Let_syntax in
+  Command.async
+    ~summary:
+      "Start the diffcessible viewer. Reads from a file or stdin if no file \
+       path is provided."
+    (let%map_open file_path =
+       flag "-file" (optional string)
+         ~doc:
+           "FILE Path to the file containing the Git diff. Reads from stdin if \
+            not provided."
+     in
+     main file_path)
 
-let () = exit (Cmd.eval cmd)
+let () =
+  Command_unix.run
+    (Command.group ~summary:"Render Git diffs in an accessible way."
+       [ ("diffcessible", command) ])
+
+(* let main () = *)
+(*   let file = In_channel.open_bin "diff.example" in *)
+(*   let s = In_channel.input_all file in *)
+(*   let patch = Patch.to_diffs s in *)
+(*   Interactive_viewer.start patch *)
+(**)
+(* open Cmdliner *)
+(**)
+(* let cmd = *)
+(*   let doc = "Render Git diffs in an accessible way." in *)
+(*   let info = Cmd.info "diffcessible" ~version:"VERSION" ~doc in *)
+(*   Cmd.v info Term.(const main $ const ()) *)
+(**)
+(* let () = exit (Cmd.eval cmd) *)

c0bb4126cd6860ad57ec03b7704615d1dd06dd4c
You Guang <yokurang@gmail.com>
2024-07-01 23:35:09 +0800

    fixing formatting

--- lib/HelpView.mli
+++ lib/HelpView.mli
@@ -6,5 +6,8 @@ val help_visible : bool Lwd.var
 val help_panel : Nottui.ui
 (** [help_panel] constructs the user interface for the help panel. This includes various help-related commands and their corresponding visual attributes, making it easy for users to understand available interactions. *)
 
+val help_keyboard_area : Nottui.ui
+(** [help_keyboard_area] provides a keyboard interaction area where pressing 'q' will close the help panel. *)
+
 val toggle_help_visibility : unit -> unit
 (** [toggle_help_visibility ()] toggles the visibility of the help panel. This function modifies the state of [help_visible], effectively showing or hiding the help panel based on its current state. *)

04626e7872de01cb113524603250879c39bd6931
You Guang <yokurang@gmail.com>
2024-07-09 00:23:27 +0800

    refactoring using fun protect

--- bin/main.ml
+++ bin/main.ml
@@ -13,15 +13,23 @@ let with_input_fd file_path f =
   | None -> f In_channel.stdin
 
 let with_setup_term f =
-  let tty_path = ttyname Unix.stdout in
-  let tty_fd = Unix.openfile tty_path [ Unix.O_RDWR ] 0o600 in
-  let term = Notty_unix.Term.create ~input:tty_fd ~output:tty_fd () in
   let is_tty = Unix.isatty Unix.stdin in
-  Fun.protect
-    (fun () -> f is_tty term)
-    ~finally:(fun () ->
-      Notty_unix.Term.release term;
-      Unix.close tty_fd)
+  if is_tty then
+    let term =
+      Notty_unix.Term.create ~input:Unix.stdin ~output:Unix.stdout ()
+    in
+    Fun.protect
+      (fun () -> f is_tty term)
+      ~finally:(fun () -> Notty_unix.Term.release term)
+  else
+    let tty_path = ttyname Unix.stdout in
+    let tty_fd = Unix.openfile tty_path [ Unix.O_RDWR ] 0o600 in
+    let term = Notty_unix.Term.create ~input:tty_fd ~output:tty_fd () in
+    Fun.protect
+      (fun () -> f is_tty term)
+      ~finally:(fun () ->
+        Notty_unix.Term.release term;
+        Unix.close tty_fd)
 
 let read_input ic =
   try In_channel.input_all ic with _ -> create_empty_diff_content ()
@@ -29,24 +37,18 @@ let read_input ic =
 let main file_path =
   with_setup_term (fun is_tty term ->
       let input_content =
-        if is_tty then
-          match file_path with
-          | Some _ -> with_input_fd file_path read_input
-          | None -> create_empty_diff_content ()
-        else
-          Fun.protect
-            (fun () -> read_input In_channel.stdin)
-            ~finally:(fun () -> In_channel.close In_channel.stdin)
+        match file_path with
+        | Some path -> with_input_fd (Some path) read_input
+        | None ->
+            if is_tty then create_empty_diff_content ()
+            else read_input In_channel.stdin
       in
       let patch = Patch.to_diffs input_content in
       InteractiveViewer.start ~term patch)
 
 let file_arg =
-  let doc =
-    "Path to the file containing the Git diff. If not provided, reads from \
-     stdin."
-  in
-  Arg.(value & pos 0 (some string) None & info [] ~docv:"FILE" ~doc)
+  let doc = "Path to the input file." in
+  Arg.(value & opt (some string) None & info [ "f"; "file" ] ~docv:"FILE" ~doc)
 
 let cmd =
   let doc =

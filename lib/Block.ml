e32a03a4c7caec5df6996665adb03d360ad07232
You Guang <yokurang@gmail.com>
2024-07-26 02:28:40 +0800

    added comments

--- lib/Block.ml
+++ lib/Block.ml
@@ -1,5 +1,35 @@
 type 'a t = Common of 'a | Changed of { mine : 'a list; their : 'a list }
 
+(*
+Hunk:
++A
+-B
++C
+-D
+
+Interpretation: We start taking from whichever sign we see first in the diff. 
+
+Block:
+[
+  Heuristic here: We take start from deletions until we encounter another deletion or a common line
+  Note: Careful when constructing back the block to a hunk. Take note of the order. .
+  Changed {mine = [A]; their = [B]};  
+  Changed {mine = [C]; their = [D]};
+]
+
+
+Block:
+[
+  Heuristic here: We take start from deletions until we encounter another deletion or a common line
+  Changed {mine = [A]; their = []};  
+  Changed {mine = [C]; their = [B]};
+  Changed {mine = []; their = [D]};
+]
+
+
+*)
+
+(** Turns a hunk into a list of blocks by grouping consecutive changed lines. *)
 let of_hunk (hunk : 'a Patch.hunk) : 'a t list =
   let rec aux acc current_mine current_their lines =
     match lines with
